71aa413f90397e020ff91e97aa4cd6ab210e77ff
noriwokorders-crypto <noriwokorders@gmail.com>
2026-01-04 21:26:21 +0200

    Add files via upload

---
+++ ground.tsx
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="ground" tilewidth="64" tileheight="64" tilecount="32" columns="8">
+ <image source="public/sprites/ground.png" width="512" height="256"/>
+ <tile id="17">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0.75" y="62">
+    <polygon points="0,0 63.25,-63 63.5,1.5"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1.5" y="3">
+    <polygon points="0,0 58,58 0,59.5"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="25">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0.5" y="0.75">
+    <polygon points="0,0 63,63.25 62,-0.5"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="26">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2" y="2.5">
+    <polygon points="0,0 58,-1 0.5,58.5"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
