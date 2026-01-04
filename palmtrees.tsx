850daa79aaff8cb851161151f8656bf132d1f799
noriwokorders-crypto <noriwokorders@gmail.com>
2026-01-04 18:59:55 +0200

    Add files via upload

---
+++ palmtrees.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="palmtrees" tilewidth="192" tileheight="176" tilecount="1" columns="1">
+ <image source="public/sprites/deco/palmtrees.png" width="192" height="176"/>
+</tileset>
