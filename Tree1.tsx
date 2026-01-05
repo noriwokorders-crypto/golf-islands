781b53f8532ae3517e5d5a6c854670fd7f360c8b
noriwokorders-crypto <noriwokorders@gmail.com>
2026-01-05 15:56:08 +0200

    Delete Tree1.tsx

--- Tree1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Tree1" tilewidth="192" tileheight="256" tilecount="8" columns="8">
- <image source="Tree1.png" width="1536" height="256"/>
-</tileset>
