a760ec103f9b75fad215e8070423fb98e3479560
noriwokorders-crypto <noriwokorders@gmail.com>
2026-01-05 15:57:11 +0200

    Add files via upload

---
+++ bush2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="bush2" tilewidth="107" tileheight="78" tilecount="1" columns="1">
+ <image source="public/sprites/deco/bush2.png" width="107" height="78"/>
+</tileset>
